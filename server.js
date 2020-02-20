fcfddd592e24ad1e27412ec1300ec76b7440cefd
Luis Castro <luigi_789@hotmail.com>
2020-02-20 12:43:37 -0500

    Removed routes

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const databaseQuery = () => {
 			'SELECT DISTINCT hashtag FROM tweets' +
 			') subquery ORDER by random() LIMIT 1;'
 
-		pool.query(distinctRandomQuery, (err, results) => {
+		pool.query(distinctQuery, (err, results) => {
 			if (err) {
 				console.log(err);
 				reject(err);
